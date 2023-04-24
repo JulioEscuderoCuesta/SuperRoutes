a0d15b9eff79fa5457a12dc6820c44906f4cf3c2
JulioEscuderoCuesta <escuderocuestajulio@gmail.com>
2023-04-24 14:38:01 +0200

    Creando adapter para que el guía vea el estado de los senderistas

--- app/src/main/java/com/example/superroutes/RouteStartedGuide.java
+++ app/src/main/java/com/example/superroutes/RouteStartedGuide.java
@@ -1,22 +1,27 @@
 package com.example.superroutes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.superroutes.custom_classes.ParticipantsInRouteGuideAdapter;
+import com.example.superroutes.databinding.ActivityMainBinding;
+import com.example.superroutes.databinding.ActivityRouteStartedGuideBinding;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class RouteStartedGuide extends AppCompatActivity {
 
     private FirebaseDatabase database;
-
     private DatabaseReference routeProposalStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_route_started_guide);
+        @NonNull ActivityRouteStartedGuideBinding binding = ActivityRouteStartedGuideBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.listOfParticipantsRouteStartedGuide.setAdapter(new ParticipantsInRouteGuideAdapter());
         database = FirebaseDatabase.getInstance("https://superroutes-5378d-default-rtdb.europe-west1.firebasedatabase.app/");
         addRouteProposalStarted(getIntent().getStringExtra("route_proposal_code"));
 
@@ -27,3 +32,4 @@ public class RouteStartedGuide extends AppCompatActivity {
 
     }
 }
+
